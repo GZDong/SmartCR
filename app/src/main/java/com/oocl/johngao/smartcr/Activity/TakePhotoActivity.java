9f2b1990650a6366fb6771ec4c4ee8323e456249
JohnGao <johngao@gdscinsts-imac.local>
2017-12-01 13:14:19 +0800

    初步完成拍照和储存功能。

--- app/src/main/java/com/oocl/johngao/smartcr/Activity/TakePhotoActivity.java
+++ app/src/main/java/com/oocl/johngao/smartcr/Activity/TakePhotoActivity.java
@@ -3,7 +3,10 @@ package com.oocl.johngao.smartcr.Activity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
+import android.graphics.Matrix;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -14,6 +17,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -22,6 +27,7 @@ import com.oocl.johngao.smartcr.Adapter.PicListApater;
 import com.oocl.johngao.smartcr.R;
 import com.oocl.johngao.smartcr.ToolsClass.CameraPreview;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -36,6 +42,7 @@ public class TakePhotoActivity extends AppCompatActivity {
 
     public static final String TAG = "CameraSimple";
     private android.hardware.Camera mCamera;
+    private int mCameraId = android.hardware.Camera.CameraInfo.CAMERA_FACING_BACK;
     private CameraPreview mPreview;
     private Button mTakePictureBtn;
     private FrameLayout mCameraLayout;
@@ -47,14 +54,25 @@ public class TakePhotoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_take_photo);
 
-        if (ContextCompat.checkSelfPermission(TakePhotoActivity.this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(TakePhotoActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(TakePhotoActivity.this,new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        if (ContextCompat.checkSelfPermission(TakePhotoActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(TakePhotoActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(TakePhotoActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        if (!checkCameraHardware(this)) {
+            Toast.makeText(this, "相机不支持", Toast.LENGTH_SHORT).show();
+        }else {
+            mCamera = getCameraInstance();
+            mPreview = new CameraPreview(TakePhotoActivity.this, mCamera);
+            CameraPreview.setCameraDisplayOrientation(this, mCameraId, mCamera);
+            mCameraLayout = (FrameLayout) findViewById(R.id.camera_preview);
+            mCameraLayout.addView(mPreview);
         }
 
         initList();
-        mPicListApater = new PicListApater(this,mPicList);
+        mPicListApater = new PicListApater(this, mPicList);
         mRecyclerView = (RecyclerView) findViewById(R.id.picture_list);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -65,82 +83,131 @@ public class TakePhotoActivity extends AppCompatActivity {
         mTakePictureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCamera.takePicture(null,null,mPictureCallback);
+                if (mCamera == null){
+                    Log.e(TAG, "1.onCreate: this is null");
+                }
+                mCamera.takePicture(null, null, mPictureCallback);
             }
         });
-
-
     }
 
-    private void initList(){
+    private void initList() {
         mPicList = new ArrayList<>();
-        for (int i = 1;i <= 10;i ++){
+        for (int i = 1; i <= 10; i++) {
             String test = "test";
             mPicList.add(test);
         }
     }
 
     //判断相机是否支持
-    private boolean checkCameraHardware(Context context){
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+    private boolean checkCameraHardware(Context context) {
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     // 获取相机
-    public static android.hardware.Camera getCameraInstance(){
+    public static android.hardware.Camera getCameraInstance() {
         android.hardware.Camera camera = null;
-        try{
+        try {
             camera = android.hardware.Camera.open();  //打开相机
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             Log.e(TAG, "getCameraInstance: 相机打不开");
         }
         return camera;
     }
 
+    //释放相机
+    public void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
     //拍照回调
     private android.hardware.Camera.PictureCallback mPictureCallback = new android.hardware.Camera.PictureCallback() {
         @Override
-        public void onPictureTaken(byte[] data, android.hardware.Camera camera) {
+        public void onPictureTaken(final byte[] data, android.hardware.Camera camera) {
+            Date date = new Date(System.currentTimeMillis());
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmm");
+            String pictureName = simpleDateFormat.format(date) + ".png";
             File pictureDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            if (pictureDir == null){
-                Log.d(TAG,"Error creating media file, check storage permissions!");
-                return;
-            }
-            try {
-                Date date = new Date(System.currentTimeMillis());
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmm");
-                String pictureName = simpleDateFormat.format(date) + ".png";
-                FileOutputStream fos = new FileOutputStream(pictureDir + File.separator + pictureName);
-                fos.write(data);
-                fos.close();
-            }catch (FileNotFoundException e){
-                Log.d(TAG, "File not found: " + e.getMessage());
-            }catch (IOException e){
-                Log.d(TAG, "Error accessing file: " + e.getMessage());
-            }
+            final String picturePath = pictureDir + File.separator + pictureName;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    File file = new File(picturePath);
+                    try {
+                        //获取当前旋转角度，并旋转照片
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0,data.length);
+                        bitmap = rotateBitmapByDegress(bitmap,90);
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+                        bitmap.compress(Bitmap.CompressFormat.JPEG,100,bos);
+                        bos.flush();
+                        bos.close();
+                        bitmap.recycle();
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            mCamera.startPreview();
         }
     };
+    //旋转图片
+    public static Bitmap rotateBitmapByDegress(Bitmap bm, int degree){
+        Bitmap returnBm = null;
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        try{
+            returnBm = Bitmap.createBitmap(bm, 0, 0,bm.getWidth(),bm.getHeight(),matrix,true);
+        }catch (OutOfMemoryError e){
+
+        }
+        if (returnBm == null){
+            returnBm = bm;
+        }
+        if (bm != returnBm){
+            bm.recycle();
+        }
+        return returnBm;
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 1:
-                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
-                    if (!checkCameraHardware(this)){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    if (!checkCameraHardware(this)) {
                         Toast.makeText(TakePhotoActivity.this, "Can not take photo!", Toast.LENGTH_SHORT).show();
-                    }else {
-                        mCamera = getCameraInstance();
-                        mPreview = new CameraPreview(TakePhotoActivity.this,mCamera);
-                        mCameraLayout = (FrameLayout) findViewById(R.id.camera_preview);
-                        mCameraLayout.addView(mPreview);
+                    } else {
+                        if (mCamera == null){
+                            mCamera = getCameraInstance();
+                            mPreview = new CameraPreview(TakePhotoActivity.this, mCamera);
+                            CameraPreview.setCameraDisplayOrientation(this, mCameraId, mCamera);
+                            mCameraLayout = (FrameLayout) findViewById(R.id.camera_preview);
+                            mCameraLayout.addView(mPreview);
+                        }
                     }
-                }else {
+                } else {
                     Toast.makeText(this, "no permission!", Toast.LENGTH_SHORT).show();
                 }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseCamera();
     }
 }
--- app/src/main/java/com/oocl/johngao/smartcr/ToolsClass/CameraPreview.java
+++ app/src/main/java/com/oocl/johngao/smartcr/ToolsClass/CameraPreview.java
@@ -1,8 +1,10 @@
 package com.oocl.johngao.smartcr.ToolsClass;
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.AttributeSet;
+import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
 
@@ -56,5 +58,35 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    public static void setCameraDisplayOrientation(Activity activity, int cameraId, Camera camera){
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(cameraId,info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degress = 0;
+        switch (rotation){
+            case Surface.ROTATION_0:
+                degress = 0;
+                break;
+            case Surface.ROTATION_90:
+                degress = 90;
+                break;
+            case Surface.ROTATION_180:
+                degress = 180;
+                break;
+            case Surface.ROTATION_270:
+                degress = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT){
+            result = (info.orientation + degress)%360;
+            result = (360 - result) % 360;
+        }else {
+            result = (info.orientation - degress + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
     }
 }
